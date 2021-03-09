99df89d7145e6625ea107323c036f888610c8946
Sergey <sergeyvasylenko4@gmail.com>
2021-03-09 11:19:31 +0200

    made changes for php mail

--- Отправка данных на почту/mail-simple.php
+++ Отправка данных на почту/mail-simple.php
@@ -39,14 +39,14 @@ $message = "<table style='width: 50%;'>$message</table>";
 
 // От кого
 $mail->setFrom('adm@' . $_SERVER['HTTP_HOST'], 'Your best site');
- 
+
 // Кому
 foreach ( $admin_email as $key => $value ) {
 	$mail->addAddress($value);
 }
 // Тема письма
 $mail->Subject = $form_subject;
- 
+
 // Тело письма
 $body = $message;
 // $mail->isHTML(true);  это если прям верстка
